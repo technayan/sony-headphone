224ce81679af349ef1040d393a98331bb8dccbdc
dev_nayan <md.nayan8494@gmail.com>
2022-04-04 14:48:17 +0600

    Home pages's hero section created

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,14 +5,17 @@ import './Home.css'
 const Home = () => {
   return (
     <section className="hero-section mt-5">
-      <Container className='d-flex'>
+      <Container className="d-flex">
         <div className="hero-text-area text-start align-items-center d-flex">
-          <h1>
-            WH-H810 h.ear on 3 Mini <span>Wireless Headphone</span>
-          </h1>
+          <div className="hero-text">
+            <h1>
+              WH-H810 h.ear on 3 Mini <span>Wireless Headphone</span>
+            </h1>
+            <button className='hero-btn mt-3'>Shop Now</button>
+          </div>
         </div>
-        <div className="hero-img-area text-end">
-            <img className='hero-img' src="img/sony-headphone.png" alt="" />
+        <div className="hero-img-area d-flex align-items-center justify-content-end">
+          <img className="hero-img" src="img/sony-headphone.png" alt="" />
         </div>
       </Container>
     </section>
